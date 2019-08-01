a10945b945800579c2ff5c03a239e066acdd1035
Christian Rebischke <chris@nullday.de>
2019-08-01 16:22:00 +0200

    tweaked with centos files and test script
    Signed-off-by: Christian Rebischke <chris@nullday.de>

--- test-container.sh
+++ test-container.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 sudo nspawn init "$1/$2/$3"
+sleep 30s
 sudo machinectl start "$1-$2-$3"
-sleep 10
+sleep 30s
 sudo machinectl login "$1-$2-$3"
 sudo machinectl stop "$1-$2-$3"
+sleep 30s
 sudo machinectl remove "$1-$2-$3"
 sudo machinectl clean
