b971d1c6bf00d13e3f22df2869075d13619c370e
Christian Rebischke <chris@shibumi.dev>
2020-04-29 13:07:24 +0200

    change clean artefacts line

--- clean-artifacts.sh
+++ clean-artifacts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find /usr/share/nginx/html/nspawn.org/public/storage/ -iname ".mkosi-*" -print -exec rm -rf {} \+
+find /srv/www/nspawn.org/public/storage/ -iname ".mkosi-*" -print -exec rm -rf {} \+
