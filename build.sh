f023c53e8c8c4ec4a9e63fc022195014abc0ce93
Eduard Tolosa <edu4rdshl@protonmail.com>
2022-03-17 22:01:19 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ definitions_dir="./"
 
 if cd $definitions_dir; then
     for f in $(for x in *.default; do echo "${x//-*/}"; done | uniq); do
-        if ! nspawn-builder -n "$f"; then
+        if ! ./nspawn-builder -n "$f"; then
             failed_builds+="$f"
         else
             echo "Build of $f succeeded"
