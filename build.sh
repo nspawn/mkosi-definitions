7afe789f698b456d6d9a4c04222ee4fc74ea9795
Edu4rdSHL <edu4rdshl@protonmail.com>
2022-03-17 21:52:31 -0500

    Add build.sh for github actions.
    Signed-off-by: Edu4rdSHL <edu4rdshl@protonmail.com>

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+failed_builds=""
+definitions_dir="./"
+
+if cd $definitions_dir; then
+    for f in $(for x in *.default; do echo "${x//-*/}"; done | uniq); do
+        if ! nspawn-builder -n "$f"; then
+            failed_builds+="$f"
+        else
+            echo "Build of $f succeeded"
+        fi
+    done
+fi
+
+echo "Failed builds: $failed_builds"
