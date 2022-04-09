3bb0cd80263e3c85ce5107ee08e0a6b8b9069a03
Eduard Tolosa <edu4rdshl@protonmail.com>
2022-04-09 11:09:19 -0500

    Use a directory tree for image definitions.
    Signed-off-by: Eduard Tolosa <edu4rdshl@protonmail.com>

--- build.sh
+++ build.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 failed_builds=""
-definitions_dir="./"
+root_dir=$(pwd)
 
-if cd $definitions_dir; then
-    for f in $(for x in *.default; do echo "${x//-*/}"; done | uniq); do
-        if ! nspawn-builder -n "$f"; then
-            failed_builds+="$f "
-        else
-            echo "Build of $f succeeded"
-        fi
-    done
-fi
+for definitions_dir in ./*/*; do
+    if cd $definitions_dir; then
+        for f in $(for x in *.default; do echo "${x//-*/}"; done | uniq); do
+            if ! nspawn-builder -n "$f"; then
+                failed_builds+="$f "
+            else
+                echo "Build of $f succeeded"
+            fi
+        done
+    fi
+    cd "$root_dir"
+done
 
 echo "Failed builds: $failed_builds"
