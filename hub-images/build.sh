57bd39e2ee3e4a19a208c69550fbb691fa281f2d
Eduard Tolosa <edu4rdshl@protonmail.com>
2024-01-23 15:34:44 -0500

    Update build.sh

--- hub-images/build.sh
+++ hub-images/build.sh
@@ -6,8 +6,8 @@ for definitions_dir in ./*/*; do
     if [ -d "$definitions_dir"/mkosi.images ] && cd "$definitions_dir"; then
         if ! mkosi; then
             echo "mkosi failed for $definitions_dir"
-            # Always exit with 0, so that all images are built
-            exit 0
+            # Always return 0, so that all images are built
+            true
         fi
     else
         echo "Could not cd to $definitions_dir"
