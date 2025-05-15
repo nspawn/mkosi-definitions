7d9a9c6cb59346d6be36d0bd3314e537f423b09d
Septatrix <24257556+Septatrix@users.noreply.github.com>
2025-05-15 15:46:32 +0200

    Update some scripts and add preliminary CI workflow

--- check-all.py
+++ check-all.py
@@ -4,37 +4,28 @@ import subprocess
 import json
 
 matrix = {
-    "debian": ["bookwork", "sid"],
-    "ubuntu": ["jammy", "lunar", "mantic"],
-    "fedora": ["37", "38", "39"],
+    "debian": ["bookworm", "sid"],
+    "ubuntu": ["noble", "plucky"],
+    "fedora": ["41", "42", "rawhide"],
     "arch": ["rolling"],
     "opensuse": ["leap", "tumbleweed"],
-    "centos": ["9"],
+    "centos": ["9", "10"],
     "alma": ["9"],
     "rocky": ["9"],
 }
 
-output_dirs = set()
-
 # TODO maybe use SplitArtifacts=tar instead of building twice
 
 for distro, releases in matrix.items():
+    if distro == "opensuse":
+        # Failes due to outdated signing key?
+        continue
+
     for release in releases:
         print(f"Building {distro} {release}")
         # Call mkosi with the appropriate arguments
         # For demonstration, we will just print ensure the config is valid
         proc = subprocess.run(
-            ["mkosi", "-d", distro, "-r", release, "--json", "summary"],
+            ["mkosi", "-d", distro, "-r", release],
             check=True,
-            capture_output=True,
         )
-        assert proc.stderr == b"", f"Please fix mkosi config for {distro} {release}"
-        summary = json.loads(proc.stdout)
-        [image] = summary["Images"]
-        output_dir = image["OutputDirectory"]
-        assert (
-            output_dir not in output_dirs
-        ), f"Duplicate output directory: {output_dir}"
-        output_dirs.add(output_dir)
-
-print(json.dumps(list(output_dirs), indent=2))
